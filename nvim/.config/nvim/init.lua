bd124493f442e2042b2ac3e5a8b3f9c717758f66
Angus Burroughs <angusb@saltpay.co>
2024-09-02 14:41:33 +0100

    nvim: added undotree

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -342,75 +342,6 @@ require('lazy').setup({
         end
       end
 
-      -- local function custom_make_openai_spec_curl_args(opts, prompt)
-      --   local url = opts.url
-      --   local api_key = opts.api_key_name and os.getenv(opts.api_key_name)
-      --   local data = {
-      --     prompt = prompt,
-      --     model = opts.model,
-      --     temperature = 0.7,
-      --     stream = true,
-      --   }
-      --   local args = { '-N', '-X', 'POST', '-H', 'Content-Type: application/json', '-d', vim.json.encode(data) }
-      --   if api_key then
-      --     table.insert(args, '-H')
-      --     table.insert(args, 'Authorization: Bearer ' .. api_key)
-      --   end
-      --   table.insert(args, url)
-      --   return args
-      -- end
-
-
-      -- local function llama_405b_base()
-      --   dingllm.invoke_llm_and_stream_into_editor({
-      --     url = 'https://openrouter.ai/api/v1/chat/completions',
-      --     model = 'meta-llama/llama-3.1-405b',
-      --     api_key_name = 'OPEN_ROUTER_API_KEY',
-      --     max_tokens = '128',
-      --     replace = false,
-      --   }, custom_make_openai_spec_curl_args, handle_open_router_spec_data)
-      -- end
-
-      -- local function groq_replace()
-      --   dingllm.invoke_llm_and_stream_into_editor({
-      --     url = 'https://api.groq.com/openai/v1/chat/completions',
-      --     model = 'llama-3.1-70b-versatile',
-      --     api_key_name = 'GROQ_API_KEY',
-      --     system_prompt = system_prompt,
-      --     replace = true,
-      --   }, dingllm.make_openai_spec_curl_args, dingllm.handle_openai_spec_data)
-      -- end
-      --
-      -- local function groq_help()
-      --   dingllm.invoke_llm_and_stream_into_editor({
-      --     url = 'https://api.groq.com/openai/v1/chat/completions',
-      --     model = 'llama-3.1-70b-versatile',
-      --     api_key_name = 'GROQ_API_KEY',
-      --     system_prompt = helpful_prompt,
-      --     replace = false,
-      --   }, dingllm.make_openai_spec_curl_args, dingllm.handle_openai_spec_data)
-      -- end
-
-      -- local function llama405b_replace()
-      --   dingllm.invoke_llm_and_stream_into_editor({
-      --     url = 'https://api.lambdalabs.com/v1/chat/completions',
-      --     model = 'hermes-3-llama-3.1-405b-fp8',
-      --     api_key_name = 'LAMBDA_API_KEY',
-      --     system_prompt = system_prompt,
-      --     replace = true,
-      --   }, dingllm.make_openai_spec_curl_args, dingllm.handle_openai_spec_data)
-      -- end
-      --
-      -- local function llama405b_help()
-      --   dingllm.invoke_llm_and_stream_into_editor({
-      --     url = 'https://api.lambdalabs.com/v1/chat/completions',
-      --     model = 'hermes-3-llama-3.1-405b-fp8',
-      --     api_key_name = 'LAMBDA_API_KEY',
-      --     system_prompt = helpful_prompt,
-      --     replace = false,
-      --   }, dingllm.make_openai_spec_curl_args, dingllm.handle_openai_spec_data)
-      -- end
-
       local function anthropic_help()
         dingllm.invoke_llm_and_stream_into_editor({
           url = 'https://api.anthropic.com/v1/messages',
@@ -431,13 +362,8 @@ require('lazy').setup({
         }, dingllm.make_anthropic_spec_curl_args, dingllm.handle_anthropic_spec_data)
       end
 
-      -- vim.keymap.set({ 'n', 'v' }, '<leader>k', groq_replace, { desc = 'llm groq' })
-      -- vim.keymap.set({ 'n', 'v' }, '<leader>K', groq_help, { desc = 'llm groq_help' })
-      -- vim.keymap.set({ 'n', 'v' }, '<leader>L', llama405b_help, { desc = 'llm llama405b_help' })
-      -- vim.keymap.set({ 'n', 'v' }, '<leader>l', llama405b_replace, { desc = 'llm llama405b_replace' })
       vim.keymap.set({ 'n', 'v' }, '<leader>I', anthropic_help, { desc = 'llm anthropic_help' })
       vim.keymap.set({ 'n', 'v' }, '<leader>i', anthropic_replace, { desc = 'llm anthropic' })
-      -- vim.keymap.set({ 'n', 'v' }, '<leader>o', llama_405b_base, { desc = 'llama base' })
     end,
   },
   {
@@ -481,6 +407,12 @@ require('lazy').setup({
         ft = { "markdown", "Avante" },
       },
     },
+  },
+  {
+    "mbbill/undotree",
+    config = function()
+      vim.keymap.set({ 'n' }, '<leader>U', vim.cmd.UndotreeToggle)
+    end
   },
 }, {})
 
